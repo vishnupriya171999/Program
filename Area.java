c8f5cf4bbd9a0e0fcd655a36527c5dcd5db2788e
vishnupriya171999 <113516416+vishnupriya171999@users.noreply.github.com>
2022-09-14 21:22:58 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,30 @@
+
+public class Area {
+	
+	public static void main(String[] args) {
+		
+		int a = 5, b = 6, h = 8, r = 10, Square, Triangle, Rectangle;
+		double Circle;
+		Square = a * a;
+		Triangle = (b * h)/2;
+		Rectangle = a * b;
+		Circle = r * r * 3.14;
+		// Square 
+		
+		System.out.println(" Area of Square is " + Square);	
+		
+		// Triangle 
+		
+		System.out.println(" Area of Triangle is " + Triangle);	
+		
+		// Rectangle
+		
+		System.out.println(" Area of Rectangle is " + Rectangle);
+		
+		// Circle
+		
+		System.out.println(" Area of Circle is " + Circle);	
+			
+	}
+
+}
---
+++ Arithmetic.java
@@ -0,0 +1,24 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) 
+	{
+		int num1 = 30, num2 = 20;
+		
+		// Subtraction 
+		
+		System.out.println(" Subtration of two numbers is  " + (num1 - num2));
+		
+		// Multiplication
+		
+		
+		System.out.println(" Multiplication of two numbers is  " + (num1 * num2));
+		
+		// Division
+		
+		System.out.println(" Division of two numbers is  " + (num1 /num2));
+		
+	
+	
+	}
+}
