285809e89907a2ea50a7778d59009d77bd3e1703
vishnupriya171999 <113516416+vishnupriya171999@users.noreply.github.com>
2022-09-15 08:00:24 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,34 @@
+package Example;
+
+public class DataType {
+
+	public static void main(String[] args) {
+
+			int i=10;
+			byte b=125;
+			short s=567;
+			long l=876L; //DataType long
+			float f=567.5f;
+			double d=98798.56;
+			char ch='A';
+			char chr=66;//65 to 90 (A-Z)  97-122(a-z)
+			boolean bool=true;
+
+			System.out.println("integer value=" + i);
+			System.out.println("short value=" + s);
+			System.out.println("byte value=" + b);
+			System.out.println("long value=" + l);
+			System.out.println("float value=" + f);
+			System.out.println("double d=" + d);
+			System.out.println("char value=" + ch);
+			System.out.println("char value using ascii=" + chr);
+			System.out.println("boolean value=" + bool);
+
+
+			}
+
+
+
+	}
+
+
